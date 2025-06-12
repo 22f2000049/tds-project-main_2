4dfc0c52fc9bb582ff7fafd5474476ced199ddb4
22f2000049 <ranasubodh10943@gmail.com>
2025-06-12 14:23:00 +0530

    some changes

--- app.py
+++ app.py
@@ -1,2 +1,14 @@
 print("Hello world!")
 
+def faboncai(n):
+    if n <= 0:
+        return []
+    elif n == 1:
+        return [1]
+    elif n == 2:
+        return [1, 1]
+    
+    result = [1, 1]
+    for i in range(2, n):
+        result.append(result[i-1] + result[i-2])
+    return result
